c489f85f6f06225c60232ae379260d4d49634e40
stevenjohs <johannessteven9@gmail.com>
2021-02-23 18:27:28 +0700

    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+// helpers
+const elQS = el => document.querySelector(el);
+const elQSA = el => document.querySelectorAll(el);
+const on = (el, event, val) => el.addEventListener(event, val);
+
+// variables
+
+// configs
+const BASE_URL = 'http://localhost:8080/';
+
+// functions
+
+// main
+window.addEventListener('load', init);
+
+function init() {
+	load();
+	eventListener();
+}
+
+function load() {
+
+}
+
+function eventListener() {
+	
+}
